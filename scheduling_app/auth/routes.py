c9969c05beeafa26fbc2c2fd94679ec792a83d15
RogueNPC <jordanbtansay@gmail.com>
2021-03-22 14:14:29 -0700

    [ADD] modified auth routes

--- scheduling_app/auth/routes.py
+++ scheduling_app/auth/routes.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 
-from tracker_app.models import Console, Game, Genre, User
-from tracker_app.auth.forms import SignUpForm, LoginForm
-from tracker_app import bcrypt
+from scheduling_app.models import Console, Game, Genre, User
+from scheduling_app.auth.forms import SignUpForm, LoginForm
+from scheduling_app import bcrypt
 
 # Import app and db from events_app package so that we can run app
-from tracker_app import app, db
+from scheduling_app import app, db
 
 auth = Blueprint("auth", __name__)
 
