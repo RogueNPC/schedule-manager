35c82271488e6ab52ff2834a34c3782df2fecfef
RogueNPC <jordanbtansay@gmail.com>
2021-03-23 12:16:55 -0700

    [UPDATE] Syntax fix

--- scheduling_app/__init__.py
+++ scheduling_app/__init__.py
@@ -23,7 +23,7 @@ from .models import User
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return User.query.get(int(user_id))
 
 bcrypt = Bcrypt(app)
 
