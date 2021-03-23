08e81fa3d9cb335b705844331a0367270b2f88ea
RogueNPC <jordanbtansay@gmail.com>
2021-03-22 19:07:55 -0700

    [UPDATE] Fixed import classes

--- scheduling_app/auth/routes.py
+++ scheduling_app/auth/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 
-from scheduling_app.models import Console, Game, Genre, User
+from scheduling_app.models import Load, User
 from scheduling_app.auth.forms import SignUpForm, LoginForm
 from scheduling_app import bcrypt
 
